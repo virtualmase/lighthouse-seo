93b9868bb3502eea248b2e53b57e416b4fd7931d
virtualmase <virtualmase@gmail.com>
2025-06-19 01:22:12 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins: [react(), tailwindcss()],
+  root: '.', // Ensure index.html in root is used
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Explicitly set entry
     },
   },
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
